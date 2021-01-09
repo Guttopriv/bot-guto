11400560b8664bf6b2705f8e7953334c1a67cf26
Guttopriv <76894319+Guttopriv@users.noreply.github.com>
2021-01-09 11:06:13 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,47 @@
+const help = (prefix) => {
+	return `*Bem vindo ao Bot Guto*\n
+Criadores:
+*Guto o Deus vivo*/n
+💠 ʟɪsᴛᴀ ᴅᴇ ᴄᴏᴍᴀɴᴅᴏs 💠\n
+✅ *${prefix}info*: Mostra informações sobre o bot\n
+✅ *${prefix}sticker*: Transforma uma imagem, gif ou vídeo em sticker\n
+✅ *${prefix}toimg*: Converte um sticker em imagem
+_Use esse comando respondendo um sticker_\n
+✅ *${prefix}tts [lang] [texto]*: Converte texto em audio\n
+✅ *${prefix}neko*: Envia uma imagem de uma garota-gato aleatória\n
+✅ *${prefix}girl*: Envia uma imagem de uma garota aleatória\n
+✅ *${prefix}boy*: Envia uma imagem de um garoto aleatória\n
+✅ *${prefix}anime*: Envia uma imagem de anime aleatória\n
+✅ *${prefix}glitch [texto 1] [texto 2]*: Envia uma imagem de glitch com os dois textos informados\n
+✅ *${prefix}play [música]*: Envia o link da música informada\n
+✅ *${prefix}text2img [texto]*: Envia uma imagem com o texto informado\n
+✅ *${prefix}frase*: Envia uma frase filosófica/reflexiva\n
+✅ *${prefix}simg [texto]*: Pesquisa uma foto referente ao texto\n
+✅ *${prefix}chat [mensagem]*: O bot responde sua mensagem\n
+✅ *${prefix}wait*: Pesquisa o anime pela imagem\n
+✅ *${prefix}say [texto]*: O Bot repete o texto que foi mandado\n
+✅ *${prefix}tiktokstalk [usuário]*: Manda as informações da conta do usuário\n
+✅ *${prefix}lang*: Manda as linguagens disponiveis para o comando !tts\n
+💠 ɢʀᴜᴘᴏs 💠\n
+✅ *${prefix}tagall*: Menciona todos do grupo
+_Comando apenas para adms_\n
+✅ *${prefix}listadmins*: Menciona todos os adms do grupo
+_Comando apenas para adms_\n
+✅ *${prefix}clone [tag]*: Copia a foto de perfil da pessoa marcada e adiciona no bot
+_Comando apenas para adms_\n
+✅ *${prefix}add [número]*: Adiciona o número ao grupo
+_Comando apenas para adms_\n
+✅ *${prefix}kick [tag]*: Remove a pessoa marcada
+_Comando apenas para adms_\n
+✅ *${prefix}promover [tag]*: Promove a pessoa marcada a adm
+_Comando apenas para adms_\n
+✅ *${prefix}rebaixar [tag]*: Tira o adm da pessoa marcada
+_Comando apenas para adms_
+✅ *${prefix}simi [0 ou 1]*: Ativa ou desativa o comando ${prefix}chat no grupo
+_Comando apenas para adms_\n
+✅ *${prefix}welcome [0 ou 1]*: Ativa ou desativa a mensagem de boas vindas no grupo
+_Comando apenas para adms_\n`
+
+}
+
+exports.help = help
